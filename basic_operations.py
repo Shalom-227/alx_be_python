fc333366965c76d975335d8226c3dc4329b00e7d
Shalom <alaladeshalom02@gmail.com>
2024-12-14 00:11:50 +0100

    adjusted #! statement

--- basic_operations.py
+++ basic_operations.py
@@ -1,7 +1,7 @@
-#!/bin/python3
+#!/bin/python
 
-number1=10;
-number2=5;
+number1 = 10;
+number2 = 5;
 
 print(f"Addition of {number1} and {number2} is {number1 + number2}");
 print(f"Subtraction of {number1} and {number2} is {number1 - number2}");
