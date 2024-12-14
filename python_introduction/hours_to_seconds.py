6799a402cc139339c6bc2a4971415b1c16c7232e
Shalom <alaladeshalom02@gmail.com>
2024-12-14 02:12:15 +0100

    Updated hours_to_seconds.py code to satisfy task instruction

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,8 +1,6 @@
 #!/bin/python3
 
 hours = 2;
-seconds_per_min = 60;
-seconds_per_hour = 60 * 60;
-seconds = seconds_per_hour;
+seconds = 3600 * 2;
 
-print(f"{hours} hour(s) is {seconds * hours} seconds");
+print(f"{hours} hour(s) is {seconds} seconds");
