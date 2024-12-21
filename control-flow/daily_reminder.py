2415ac775c8fb3d48ecbad46b4168a7771779dd7
Shalom <alaladeshalom02@gmail.com>
2024-12-21 15:27:53 +0100

    adjusted value of message to satisfy task requirement.

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,14 +6,14 @@ time_bound=input("Is it time-bound? (yes/no): ")
 
 match priority:
 	case "high":
-		message=f"Reminder: '{task}' is a high priority task" 
+		message = f"Reminder: '{task}' is a high priority task" 
 	case "medium":
-		message=f"Reminder: '{task}' is a medium priority task"
+		message = f"Reminder: '{task}' is a medium priority task"
 	case "low":
-		message=f"Reminder: '{task}' is a low priority task" 
+		message = f"Reminder: '{task}' is a low priority task" 
 	case _:
 		print(f"Invalid input. Enter priority level (high/medium/low")
-if time_bound=='yes':
+if time_bound == 'yes':
 	print(f"{message} that requires immediate attention today!")
 else:
 	print(f"{message}. Consider completing it when you have free time.")
