bad14270fa8371a0381fcd1e48f53deec5def191
Shalom <alaladeshalom02@gmail.com>
2025-01-12 20:59:47 +0100

    defined specific methods, datetime and timedelta,  from the import keyword inorder to match task requirement

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,9 +1,10 @@
 #!/bin/python3
 
-import datetime
+from datetime import datetime
+from datetime import timedelta
 
 def display_current_datetime():
-	current_date = datetime.datetime.now()
+	current_date = datetime.now()
 	return current_date.replace(microsecond=0)
 
 print("Current date and time: ", display_current_datetime())
@@ -11,7 +12,7 @@ print("Current date and time: ", display_current_datetime())
 number_of_days = int(input("Enter the number of days to add to the current date: "))
 
 def calculate_future_date(a):
-	return datetime.timedelta(days=number_of_days)
+	return timedelta(days=number_of_days)
 
 future_date = display_current_datetime() + calculate_future_date(number_of_days)
 print("Future date: ",future_date.date())
