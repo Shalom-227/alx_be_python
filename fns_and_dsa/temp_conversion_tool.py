d368fb87e73a803db68c01c07ecaa979df9b5ec4
Shalom <alaladeshalom02@gmail.com>
2025-01-13 00:09:28 +0100

    removed brackets from global variable value

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
-CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 temperature = int(input("Enter the temperature to convert: "))
 temperature_convention = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
